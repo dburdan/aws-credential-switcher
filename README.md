9ac1d4efcee7b68401db3b9c29c33a34cf036180
Dacri Burdan <dburdan@gmail.com>
2021-02-07 13:25:50 -0800

    Add basic support for aws config file

--- README.md
+++ README.md
@@ -4,21 +4,43 @@ Simple CLI tool that allows you to quickly toggle between AWS CLI profiles. It w
 
 ![](https://github.com/dburdan/aws-credential-switcher/blob/master/assets/cli.png?raw=true)
 
-> **NOTE:** This tool updates your local `~/.aws/credentials` file. Though we create a backup, it's recommended you create manual backup first.
-
 ## 💿 Installation
 ```
 npm i -G aws-credential-switcher
 ```
 
 ## 💻 Usage
+> **NOTE:** This tool updates your local `~/.aws/credentials` and `~/.aws/config` files. Though we create a backup, it's recommended you create manual backups first.
 ```
 aws-switch
 ```
 or
 ```
-aws-switch --credentialFile {fileLocation}
+aws-switch --credentialFile {fileLocation} --configFile {fileLocation}
 ```
 
-## License
+## 🗒 Notes
+Since this tool also updates the AWS config file, it's recommended you add a config option for each profile, even if they're duplicates.
+#### `~/.aws/credentials`
+```
+[personal]
+aws_access_key_id=abcedf
+aws_secret_access_key=abcedf
+
+[work]
+aws_access_key_id=abcedf
+aws_secret_access_key=abcedf
+```
+#### `~/.aws/config`
+```
+[profile personal]
+region=us-west-2
+output=json
+
+[profile work]
+region=us-east-1
+output=json
+```
+
+## 📄 License
 [MIT](https://choosealicense.com/licenses/mit/)
--- index.ts
+++ index.ts
@@ -6,10 +6,11 @@ import yargs from 'yargs';
 
 // Check CLI arg for custom credential file path
 const customCredFileLocation = yargs?.argv?.credentialFile as string;
+const customConfigFileLocation = yargs?.argv?.configFile as string;
 
 
 /**
- * Load and parse credential file
+ * Load and parse credential/config file
  */
 const credentialFile = customCredFileLocation || `${homedir}/.aws/credentials`;
 const credentialFileFirstBackup = `${credentialFile}.switcher.firstbck`;
@@ -36,38 +37,77 @@ const currentProfile = Object.entries(credentialSet).find(([key, values]) => {
   return values?.aws_access_key_id === credentialSet?.default?.aws_access_key_id;
 })?.[0];
 
+const configFile = customConfigFileLocation || `${homedir}/.aws/config`;
+const configFileFirstBackup = `${configFile}.switcher.firstbck`;
+const configFileBackup = `${configFile}.switcher.bck`;
+const configFileContent = readFileSync(configFile, { encoding: 'utf-8' });
+const configSet: Record<string, Record<string, string>> = {};
 
-/**
- * Check if first backup created, if not, create
- */
-if (!existsSync(credentialFileFirstBackup)) {
-  copyFileSync(credentialFile, credentialFileFirstBackup);
-}
+let nConfig = '';
+configFileContent.split('\n').forEach((line) => {
+  const firstChar = line[0];
 
-
-/**
- * List profiles, show current, ask for new, update credentials
- */
-const profiles = Object.keys(credentialSet).filter((k) => k !== 'default');
-inquirer.prompt([
-  {
-    name: 'profile',
-    message: 'Select a profile',
-    suffix: `  - Current: ${currentProfile}`,
-    type: 'list',
-    choices: profiles,
+  // Profile name line
+  if (line[0] === '[') {
+    const profileName = line.substr(1, line.length - 2).replace('profile ', '');
+    nConfig = profileName;
+    configSet[profileName] = {};
+  } else if (firstChar !== '#' && line.trim() !== '') {
+    const [key, value] = line.split('=');
+    configSet[nConfig][key] = value;
   }
-]).then(({ profile }) => {
-  // Create backup for this run
-  copyFileSync(credentialFile, credentialFileBackup);
-
-  // Generate new credential string
-  const entries = Object.entries(credentialSet[profile]).map(([key, value]) => {
-    return `${key}=${value}`;
-  }).join('\n');
-  const newCreds = `[default]\n${entries}\n\n`;
-
-  // Switch profile by overwriting default section with current settings
-  const newFile = credentialFileContent.replace(/\[default([^[])*/gs, newCreds);
-  writeFileSync(credentialFile, newFile, { encoding: 'utf-8' });
 });
+
+
+if (currentProfile) {
+  /**
+   * Check if first backup created, if not, create
+   */
+  if (!existsSync(credentialFileFirstBackup)) {
+    copyFileSync(credentialFile, credentialFileFirstBackup);
+  }
+  if (!existsSync(configFileFirstBackup)) {
+    copyFileSync(configFileBackup, configFileFirstBackup);
+  }
+
+
+  /**
+   * List profiles, show current, ask for new, update credentials
+   */
+  const profiles = Object.keys(credentialSet).filter((k) => k !== 'default');
+  inquirer.prompt([
+    {
+      name: 'profile',
+      message: 'Select a profile',
+      suffix: `  - Current: ${currentProfile}`,
+      type: 'list',
+      choices: profiles,
+    }
+  ]).then(({ profile }) => {
+    // Create backups for this run
+    copyFileSync(credentialFile, credentialFileBackup);
+    copyFileSync(configFile, configFileBackup);
+
+    // Generate new credential string
+    const credentialEntries = Object.entries(credentialSet[profile]).map(([key, value]) => {
+      return `${key}=${value}`;
+    }).join('\n');
+    const newCreds = `[default]\n${credentialEntries}\n\n`;
+
+    // Switch profile by overwriting default section with current settings
+    const newCredFile = credentialFileContent.replace(/\[default([^[])*/gs, newCreds);
+    writeFileSync(credentialFile, newCredFile, { encoding: 'utf-8' });
+
+    // Generate new config string
+    const configEntries = Object.entries(configSet[profile]).map(([key, value]) => {
+      return `${key}=${value}`;
+    }).join('\n');
+    const newConfig = `[default]\n${configEntries}\n\n`;
+
+    // Switch config by overwriting default section with current config
+    if (configSet?.[currentProfile]) {
+      const newConfigFile = configFileContent.replace(/\[default([^[])*/gs, newConfig);
+      writeFileSync(configFile, newConfigFile, { encoding: 'utf-8' });
+    }
+  });
+}
